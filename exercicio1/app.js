1586d467740881c979a66f846fd1d7745378ab08
Leeh <leticiasouza56000@gmail.com>
2026-02-11 23:09:01 -0300

    Update personal information in app.js

--- exercicio1/app.js
+++ exercicio1/app.js
@@ -1,5 +1,5 @@
-console.log("Meu nome é: Luiz miguel santos almeida")
-console.log("Minha idade é: 18 anos")
+console.log("Meu nome é: Letícia de Souza nascimento")
+console.log("Minha idade é: 17 anos")
 var soma = 7+3
 console.log("7 + 3 é: " + soma )
 
