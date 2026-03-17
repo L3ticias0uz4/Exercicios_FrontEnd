ff8a0b528e5526a8f6fe94ba35c584b559e082a6
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-03-17 10:13:09 -0300

    new exercice

---
+++ exercicio6/app.js
@@ -0,0 +1,16 @@
+function comprimento(nome, idade) {
+
+    console.log(`Oii ${nome}, você tem ${idade} anos de idade`);   
+    
+}
+const dobro = function(a) {
+    return a * 2;
+}
+
+const soma =  (b,c) => b + c;
+
+comprimento("Letícia", 16)
+
+console.log( "Dobro de 209:", dobro(209));
+
+console.log("Soma:", soma(10,24));
